26eac3a4bdbb558ee722ece07fe09aca6c034002
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-05-07 23:11:43 +0300

    class27done

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./main.scss";
 import Form from "../form/form";
 import Result from "../result/result";
 import {useState} from "react";
+import axios from "axios";
 
 function Main() {
     const [result, setResult] = useState({});
-    console.log(result);
+    const [data, setData] = useState(null);
+    const [headers, setHeaders] = useState({});
+    const[loading, setLoading] = useState(false);
+
+    const Loadingfunction = () => {
+        return new Promise((resolve) => setTimeout(resolve, 1500));
+
+    }
+
+  
+    useEffect(() => {
+        if(loading){
+            Loadingfunction().then(() => {
+                setLoading(false);
+            });
+        };
+        }, [loading]);
+    //   console.log(result);
     const Method = (props) => {
         setResult(props);
     }
+    const sendData = async (props) => {
+            setLoading(true);
+            var chosenMethod = props.method.toLowerCase();
+            const response = await axios[chosenMethod](props.url,(props.body)? (props.body) : null);
+            let result = {
+                Results : response.data,
+            }
+            setData(result);
+            var headers = {Headers :response.headers};
+            setHeaders(headers);
+    }
+
     return (
         <div className="main">
-            <Form methodSend = {Method} />
-            <Result Method={result.method} url={result.url}/>
+            <Form methodSend = {Method} sendData={sendData}/>
+            <Result Method={result.method} url={result.url} data={data} headers={headers} loading={loading}/>
         </div>
+      
 
     )
 }
--- src/component/result/result.js
+++ src/component/result/result.js
@@ -2,7 +2,24 @@ import React from 'react';
 import './result.scss';
 import JSONPretty from 'react-json-pretty';
 function Result(props) {
+if(!props.loading){
+     return (
+        <div className="result">
+            <div className="result-header">
+                <p>
+                    {props.Method}  {props.url}
+                </p>
+            </div>
+            <div className="result-body">
 
+             <JSONPretty data={props.headers}/>
+                
+             <JSONPretty data={props.data}/>
+            
+            </div>
+        </div>
+    )
+} else {
     return (
         <div className="result">
             <div className="result-header">
@@ -11,12 +28,13 @@ function Result(props) {
                 </p>
             </div>
             <div className="result-body">
-             <JSONPretty>
- 
-             </JSONPretty>
+
+           <p>Loading...</p>
             
             </div>
         </div>
     )
+}
+   
 }
 export default Result;
